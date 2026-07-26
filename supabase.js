ab2a6857f371e520cabd29692d33c7b1f0a92769
SINFORMATICA <fdngola1@gmail.com>
2026-07-25 23:02:17 -0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ewcfvjnnbxviudhtagxh.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_8Y5K0vFOIqyHeVS4NuLwdg_hvLx5HDx";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
