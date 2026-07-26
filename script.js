a43396bca17a019918e487f01a23119d94ac3185
SINFORMATICA <fdngola1@gmail.com>
2026-07-26 00:25:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
     'use strict';
 
     // ─── Supabase Config ───
-    const SUPABASE_URL = 'https://bwwkftxdblxmioghjldu.supabase.co';
-    const SUPABASE_ANON_KEY = 'sb_publishable_U6N79tV3sToBI-iz_OXLAg_TSBR2VyI';
+    const SUPABASE_URL = 'https://gxncdimqptgkdsuxpqnt.supabase.co';
+    const SUPABASE_ANON_KEY = 'sb_publishable_QyJRrWHSBPXKZecoPeEVjw_GrHwCYzh';
     const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
         auth: {
             autoRefreshToken: true,
