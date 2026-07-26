578aefc090226f2a4648c24948a271ae36852bfe
SINFORMATICA <fdngola1@gmail.com>
2026-07-25 23:06:43 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = "https://ewcfvjnnbxviudhtagxh.supabase.co";
+const SUPABASE_URL = "https://gxncdimqptgkdsuxpqnt.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_8Y5K0vFOIqyHeVS4NuLwdg_hvLx5HDx";
+const SUPABASE_ANON_KEY = "sb_publishable_QyJRrWHSBPXKZecoPeEVjw_GrHwCYzh";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
